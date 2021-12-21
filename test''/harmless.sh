aecd8b13725e5897fa315e6cb47e423bc7d048a5
kusky <37901668+kusky3@users.noreply.github.com>
2021-12-21 11:58:09 +0100

    Update harmless.sh

--- test''/harmless.sh
+++ test''/harmless.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "This script is harmless please copy its raw and url run 'curl -s $URL | bash'"
+echo "This script is harmless please copy its raw url and run 'curl -s $URL | bash'"
